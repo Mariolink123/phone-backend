33e2c191f096b0b4a43426e412befca3864c6b5e
Aneel <aneel.amjad.2019@uni.strath.ac.uk>
2022-06-23 14:24:25 +0100

    uibuild

--- index.js
+++ index.js
@@ -97,10 +97,11 @@ app.get('/api/persons/:id', (request, response) => {
 
 
 app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
-
+  Person.findByIdAndRemove(request.params.id)
+  .then(result => {
     response.status(204).end()
+  })
+  .catch(error => next(error))
 })
 
 const unknownEndpoint = (request, response) => {
