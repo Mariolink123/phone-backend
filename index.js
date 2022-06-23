0b6eebb54b3b03a4fa0fad6609c12acb9eebc2ff
Aneel <aneel.amjad.2019@uni.strath.ac.uk>
2022-06-23 15:54:35 +0100

    uibuild

--- index.js
+++ index.js
@@ -5,29 +5,6 @@ require('dotenv').config()
 const morgan = require('morgan')
 const Person = require('./models/person')
 
-/*let persons = [
-    {
-        "id": 1,
-        "name": "Arto Hellas",
-        "number": "040-123456"
-    },
-    {
-        "id": 2,
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523"
-    },
-    {
-        "id": 3,
-        "name": "Dan Abramov",
-        "number": "12-43-234345"
-    },
-    {
-        "id": 4,
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122"
-    }
-]*/
-
 app.use(express.static('build'))
 
 app.use(cors())
@@ -54,6 +31,16 @@ app.post('/api/persons', (request, response) => {
         error: 'content missing' 
       })
     }
+
+    Person.find({}).then(persons => {
+      if (persons.find(p => p.name === body.name)) {
+          return response.status(400).json({
+          error: 'name must be unique'
+      })
+      }
+    })
+
+    console.log("we continue")
 
     /*if (persons.find(p => p.name === body.name)) {
         return response.status(400).json({
@@ -90,11 +77,18 @@ app.get('/api/persons', (req, res) => {
 })
 
 app.get('/api/persons/:id', (request, response) => {
-  Person.findById(request.params.id).then(person => {
-    response.json(person)
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => {
+      next(error)
+    })
   })
-})
-
 
 app.delete('/api/persons/:id', (request, response) => {
   Person.findByIdAndRemove(request.params.id)
@@ -104,11 +98,38 @@ app.delete('/api/persons/:id', (request, response) => {
   .catch(error => next(error))
 })
 
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
+
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
 
 app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 
 const PORT = process.env.PORT || 3001
